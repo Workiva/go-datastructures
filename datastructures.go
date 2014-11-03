9cd71ce3f7241a7cd8d20befbe803258bf873237
Dustin Hiatt <dustin.hiatt@workiva.com>
2014-11-03 16:03:05 -0600

    Making it easier to import.

---
+++ datastructures.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "github.com/Workiva/go-datastructures/augmentedtree"
+	_ "github.com/Workiva/go-datastructures/bitarray"
+	_ "github.com/Workiva/go-datastructures/futures"
+	_ "github.com/Workiva/go-datastructures/graph"
+	_ "github.com/Workiva/go-datastructures/rangetree"
+	_ "github.com/Workiva/go-datastructures/set"
+	_ "github.com/Workiva/go-datastructures/threadsafe/err"
+)
