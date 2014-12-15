9b51bcf4d85215df385ab20fed0239808eed4f92
Alexander Campbell <alexander.campbell@workiva.com>
2014-12-15 10:20:12 -0600

    Add queue and slice, remove graph

--- datastructures.go
+++ datastructures.go
@@ -11,8 +11,9 @@ import (
 	_ "github.com/Workiva/go-datastructures/augmentedtree"
 	_ "github.com/Workiva/go-datastructures/bitarray"
 	_ "github.com/Workiva/go-datastructures/futures"
-	_ "github.com/Workiva/go-datastructures/graph"
+	_ "github.com/Workiva/go-datastructures/queue"
 	_ "github.com/Workiva/go-datastructures/rangetree"
 	_ "github.com/Workiva/go-datastructures/set"
+	_ "github.com/Workiva/go-datastructures/slice"
 	_ "github.com/Workiva/go-datastructures/threadsafe/err"
 )
