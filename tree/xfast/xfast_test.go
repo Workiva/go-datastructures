7f6e44dad3d310acb162188513c75b1e126980d4
Dustin Hiatt <dustin.hiatt@workiva.com>
2015-01-13 10:58:05 -0600

    More docstrings and added a "Get" method.

--- tree/xfast/xfast_test.go
+++ tree/xfast/xfast_test.go
@@ -1,8 +1,30 @@
+/*
+Copyright 2014 Workiva, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Package err implements a threadsafe error interface.  In my places,
+I found myself needing a lock to protect writing to a common error interface
+from multiple go routines (channels are great but slow).  This just makes
+that process more convenient.
+*/
+
 package xfast
 
 import (
 	"fmt"
-	"log"
 	"math"
 	"testing"
 
@@ -10,10 +32,6 @@ import (
 
 	"github.com/Workiva/go-datastructures/slice"
 )
-
-func init() {
-	log.Printf(`I HATE THIS`)
-}
 
 func whichSide(n, parent *node) int {
 	if parent.children[0] == n {
@@ -154,6 +172,15 @@ func TestInsert(t *testing.T) {
 	assert.Equal(t, e1, xft.Min())
 	assert.Equal(t, e2, xft.Max())
 	checkTrie(t, xft)
+}
+
+func TestGet(t *testing.T) {
+	xft := New(uint8(0))
+	e1 := newMockEntry(5)
+	xft.Insert(e1)
+
+	assert.Equal(t, e1, xft.Get(5))
+	assert.Nil(t, xft.Get(6))
 }
 
 func TestInsertOverwrite(t *testing.T) {
