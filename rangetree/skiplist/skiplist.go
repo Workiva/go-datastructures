f88828b42b8fae716a9b1a7cb48f6c045aca2b1b
Dustin Hiatt <dustin.hiatt@workiva.com>
2015-01-27 16:18:04 -0600

    Added package comments.

--- rangetree/skiplist/skiplist.go
+++ rangetree/skiplist/skiplist.go
@@ -1,3 +1,36 @@
+/*
+Copyright 2014 Workiva, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Package skiplist implements an n-dimensional rangetree based on a skip
+list.  This should be faster than a straight slice implementation as
+memcopy is avoided.
+
+Time complexities revolve around the ability to quickly find items
+in the n-dimensional skiplist.  That time can be defined by the
+number of items in any dimension.  Let N1, N2,... Nn define the
+number of dimensions.
+
+Performance characteristics:
+Space: O(n)
+Search: O(log N1 + log N2 + ...log Nn) = O(log N1*N2*...Nn)
+Insert: O(log N1 + log N2 + ...log Nn) = O(log N1*N2*...Nn)
+Delete: O(log N1 + log N2 + ...log Nn) = O(log N1*N2*...Nn)
+*/
+
 package skiplist
 
 import (
