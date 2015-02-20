7c7b78c7c9a876b01ea97ebe0c227c292006f635
Dustin Hiatt <dustin.hiatt@workiva.com>
2015-02-20 16:43:48 -0600

    Added some documentation.

--- rtree/hilbert/tree.go
+++ rtree/hilbert/tree.go
@@ -1,7 +1,22 @@
+/*
+Copyright 2014 Workiva, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package hilbert
 
 import (
-	"log"
 	"runtime"
 	"sync"
 	"sync/atomic"
@@ -10,17 +25,12 @@ import (
 	"github.com/Workiva/go-datastructures/rtree"
 )
 
-func init() {
-	log.Printf(`I HATE THIS.`)
-}
-
 type operation int
 
 const (
 	get operation = iota
 	add
 	remove
-	apply
 )
 
 const multiThreadAt = 1000 // number of keys before we multithread lookups
--- rtree/interface.go
+++ rtree/interface.go
@@ -1,8 +1,28 @@
+/*
+Copyright 2014 Workiva, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package rtree
 
+// Rectangles is a typed list of Rectangle.
 type Rectangles []Rectangle
 
+// Rectangle describes a two-dimensional bound.
 type Rectangle interface {
+	// LowerLeft describes the lower left coordinate of this rectangle.
 	LowerLeft() (int32, int32)
+	// UpperRight describes the upper right coordinate of this rectangle.
 	UpperRight() (int32, int32)
 }
